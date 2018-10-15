6904bc6275d62bfaa38815545fc7707a78f7a9eb
some-subhra <somesubhrapatra@gmail.com>
2018-10-15 21:45:54 +0530

    Add files via upload
    files added

---
+++ index.js
@@ -0,0 +1,123 @@
+/**
+ * This program is a boliler plate code for the famous tic tac toe game
+ * Here box represents one placeholder for either X or a 0
+ * We have a 2D array to represent the arrangement of X or O is a grid
+ * 0 -> empty box
+ * 1 -> box with X
+ * 2 -> box with O
+ * 
+ * Below are the tasks which needs to be completed
+ * Imagine you are playing with Computer so every alternate move should be by Computer
+ * X -> player
+ * O -> Computer
+ * 
+ * Winner has to be decided and has to be flashed
+ * 
+ * Extra points will be given for the Creativity
+ * 
+ * Use of Google is not encouraged
+ * 
+ */
+const grid = [];
+const GRID_LENGTH = 3;
+let turn = 'X';
+let moves = 1;
+
+function initializeGrid() {
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
+        const tempArray = [];
+        for (let rowidx = 0; rowidx < GRID_LENGTH; rowidx++) {
+            tempArray.push(0);
+        }
+        grid.push(tempArray);
+    }
+}
+
+function getRowBoxes(colIdx) {
+    let rowDivs = '';
+
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
+        let additionalClass = 'darkBackground';
+        let content = '';
+        const sum = colIdx + rowIdx;
+        if (sum % 2 === 0) {
+            additionalClass = 'lightBackground'
+        }
+        const gridValue = grid[colIdx][rowIdx];
+        if (gridValue === 1) {
+            content = '<span class="cross">X</span>';
+        }
+        else if (gridValue === 2) {
+            content = '<span class="cross">O</span>';
+        }
+        rowDivs = rowDivs + '<div colIdx="' + colIdx + '" rowIdx="' + rowIdx + '" class="box ' +
+            additionalClass + '">' + content + '</div>';
+    }
+    return rowDivs;
+}
+
+function getColumns() {
+    let columnDivs = '';
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
+        let coldiv = getRowBoxes(colIdx);
+        coldiv = '<div class="rowStyle">' + coldiv + '</div>';
+        columnDivs = columnDivs + coldiv;
+    }
+    return columnDivs;
+}
+
+function renderMainGrid() {
+    const parent = document.getElementById("grid");
+    const columnDivs = getColumns();
+    parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+}
+
+function onBoxClick() {
+    var rowIdx = this.getAttribute("rowIdx");
+    var colIdx = this.getAttribute("colIdx");
+    if (!grid[colIdx][rowIdx]) {
+        let newValue = 1;
+        if (moves % 2 === 0) {
+            newValue = 2;
+        }
+        grid[colIdx][rowIdx] = newValue;
+        renderMainGrid();
+        addClickHandlers();
+        moves++;
+        checkWinner(grid);
+    }
+}
+
+function checkWinner(grid) {
+    // console.log("grid - ", grid);
+    // r1, r2, r3, c1, c2, c3, d1, d2
+    for (i = 0; i < grid.length; i++) {
+        let rowFind = 0;
+        let colFind = 0;
+        for (j = 0; j < grid[i].length; j++) {
+            rowFind += grid[i][j];
+            colFind += grid[j][i];
+        }
+
+        if (rowFind === 3 || rowFind === 6) {
+            console.log((rowFind === 3 ? 'X' : 'O') + " wins");
+            break;
+        }
+        
+        if (colFind === 3 || colFind === 6) {
+            console.log((colFind === 3 ? 'X' : 'O') + " wins");
+            break;
+        }
+    }
+}
+
+function addClickHandlers() {
+    var boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        boxes[idx].addEventListener('click', onBoxClick, false);
+    }
+}
+
+initializeGrid();
+renderMainGrid();
+addClickHandlers();
